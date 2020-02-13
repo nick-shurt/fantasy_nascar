c186574528c4cd12052ef64c75ad03f59dd5329c
Nick Shurtleff <nicholas.shurtleff@myccmortgage.com>
2020-02-13 16:53:23 -0500

    Testing isolating just the main races before inserting into database

--- insertRaces.php
+++ insertRaces.php
@@ -1,6 +1,19 @@
 <?php
 echo "Inserting Races...";
+echo "<br>";
+echo "<br>";
 
 $xml=simplexml_load_file("2020_cup_schedule.xml");
-echo $xml->season->event[0]["name"];
+
+$k = 1;
+foreach ($xml->season->event as $event) {
+    foreach ($event->race as $race) {
+        if (isset($race["number"])) {
+            echo $k . ". " . $race["name"];
+            echo "<br>";
+            $k++;
+        }
+    }
+}
+
 ?>
