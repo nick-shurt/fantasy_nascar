679ede63a8b3bafe2c9c65a4f124417653c48bd8
Nick Shurtleff <nicholas.shurtleff@myccmortgage.com>
2020-05-28 19:44:04 -0400

    Manually updating current week for scoreboard (week 8)

--- fantasy_nascar.php
+++ fantasy_nascar.php
@@ -281,7 +281,7 @@
     <script>
     $(function() {
         //var optionValue  = "<?php get_current_week(); ?>";
-        var optionValue = "seven";
+        var optionValue = "eight";
         $("#theSelect").val(optionValue)
         .find("option[value=" + optionValue +"]").attr('selected', true);
     })
