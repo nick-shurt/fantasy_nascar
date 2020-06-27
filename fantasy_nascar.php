31ccef3bd9c0f57f7109666395587d98ce369cec
Nick Shurtleff <nicholas.shurtleff@myccmortgage.com>
2020-06-27 12:59:02 -0400

    Manually updating current week for scoreboard (week 14)

--- fantasy_nascar.php
+++ fantasy_nascar.php
@@ -281,7 +281,7 @@
     <script>
     $(function() {
         //var optionValue  = "<?php get_current_week(); ?>";
-        var optionValue = "thirteen";
+        var optionValue = "fourteen";
         $("#theSelect").val(optionValue)
         .find("option[value=" + optionValue +"]").attr('selected', true);
     })
