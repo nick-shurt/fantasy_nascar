099cfbb4d1299f65ed1cbdc799aa48bcaf373cfd
Nicholas Shurtleff <Nicholas.Shurtleff@myccmortgage.com>
2020-07-23 09:38:39 -0400

    Manually updating scoreboard current week to 19

--- fantasy_nascar.php
+++ fantasy_nascar.php
@@ -281,7 +281,7 @@
     <script>
     $(function() {
         //var optionValue  = "<?php get_current_week(); ?>";
-        var optionValue = "eighteen";
+        var optionValue = "nineteen";
         $("#theSelect").val(optionValue)
         .find("option[value=" + optionValue +"]").attr('selected', true);
     })
