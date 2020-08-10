eb19539d9dc7b8513bd0a5a2eea131a9dcc160dd
Nick Shurtleff <nicholas.shurtleff@myccmortgage.com>
2020-08-10 08:36:26 -0400

    Manually updating scoreboard week to 22

--- fantasy_nascar.php
+++ fantasy_nascar.php
@@ -281,7 +281,7 @@
     <script>
     $(function() {
         //var optionValue  = "<?php get_current_week(); ?>";
-        var optionValue = "twenty-one";
+        var optionValue = "twenty-two";
         $("#theSelect").val(optionValue)
         .find("option[value=" + optionValue +"]").attr('selected', true);
     })
